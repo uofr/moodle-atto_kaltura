6ae2c9ad3096fe7d22232892a450cbf7ef1c1e90
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2019-08-29 03:28:40 +0900

    Update

---
+++ lang/en/atto_yukaltura.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_yukaltura', language 'en'.
+ *
+ * @package   atto_yukaltura
+ * @copyright (C) 2019 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'YU Kaltura Media';
+
+// Capability strings.
+$string['yukaltura:view'] = 'View YU Kaltura tool buttons.';
+
+$string['form_title'] = 'YU Kaltura Media';
+$string['insert'] = 'Insert';
+$string['cancel'] = 'Cancel';
+$string['insert_media'] = 'Please click Insert button below to embed this media.';
+$string['selector_button_title'] = 'Embed existing media';
+$string['uploader_button_title'] = 'Uplaod and embed media';
+$string['recorder_button_title'] = 'Record and embed media (PC only)';
