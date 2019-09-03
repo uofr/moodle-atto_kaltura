f21e743cd6dfde5f57fc15ab33f66e009ffa8cc7
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2019-09-03 21:28:05 +0900

    Update

--- atto_selector.php
+++ atto_selector.php
@@ -17,9 +17,9 @@
 /**
  * Embed media selector script
  *
- * @package    atto_yukaltura
- * @copyright  (C) 2019 Yamaguchi University (gh-cc@mlex.cc.yamaguchi-u.ac.jp)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   atto_yukaltura
+ * @copyright (C) 2019 Yamaguchi University (gh-cc@mlex.cc.yamaguchi-u.ac.jp)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . '/config.php');
